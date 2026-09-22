9b924ec2f80bd7354efed641e3a77b649a75a94d
Azra Javed <azraj063@gmail.com>
2026-09-22 10:45:09 +0500

    feat: add Profiles module and controller with initial tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { UsersModule } from './users/users.module.js';
+import { ProfilesModule } from './profiles/profiles.module.js';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProfilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
