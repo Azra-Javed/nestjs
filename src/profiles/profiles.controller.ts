e33ca8bc8605bed78a680579757151a71023e25b
Azra Javed <azraj063@gmail.com>
2026-09-23 13:39:35 +0500

    feat: add CreateProfileDto and implement POST method in ProfilesController

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,11 +1,31 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { CreateProfileDto } from './dto/create-profile.dto.js';
+
 
 @Controller('profiles')
 export class ProfilesController {
     //@path: Get/profiles
 
     @Get()
-    findAll(@Query('age') age:number) {
+    findAll(@Query('age') age: number) {
         return [age];
     }
+
+    //@path: GET/profiles/:id
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return { id };
+    }
+
+    //@path: POST /profiles
+    @Post()
+    create(@Body() body: CreateProfileDto) {
+        return {
+            name: body.name,
+            age: body.age,
+        }
+
+    }
 }
+
